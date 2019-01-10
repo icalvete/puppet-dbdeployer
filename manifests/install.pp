f63b70274392de7f0be722a499c890a5a1f50ab2
Israel Calvete <icalvete@gmail.com>
2019-01-10 18:55:57 +0100

    final release

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,7 +10,7 @@ class dbdeployer::install {
 
   wget::fetch { 'download_dbdeployer':
     source      => "https://github.com/datacharmer/dbdeployer/releases/download/v1.16.0/dbdeployer-1.16.0.linux.tar.gz",
-    destination => $dbdeployer::install_dir,
+    destination => "${dbdeployer::install_dir}/dbdeployer-1.16.0.linux.tar.gz",
     timeout     => 0,
     verbose     => true,
     require     => File['dbdeployer_install_dir']
@@ -19,6 +19,15 @@ class dbdeployer::install {
   exec { 'extract_download_dbdeployer':
     command => "/bin/tar xfz ${dbdeployer::install_dir}/dbdeployer-1.16.0.linux.tar.gz",
     user    => 'root',
+    cwd     => $dbdeployer::install_dir,
+    unless  => "/usr/bin/test -f ${dbdeployer::install_dir}/dbdeployer-1.16.0.linux",
     require => Wget::Fetch['download_dbdeployer']
+  }
+  
+  file { "symlink_dbdeployer":
+    ensure  => link,
+    path    => "/usr/bin/dbdeployer-1.16.0.linux",
+    target  => "${dbdeployer::install_dir}/dbdeployer-1.16.0.linux",
+    require => Exec['extract_download_dbdeployer']
   }
 }
